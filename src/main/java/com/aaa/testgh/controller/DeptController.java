67f8c559e44c7365a3f56de634f147ae321f48fd
gwdqy88 <1649505635@qq.com>
2018-11-29 09:32:10 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,16 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:gwd
+ * createTime:2018-11-29 09:06
+ */
+
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交");
+    }
+
+
+}
