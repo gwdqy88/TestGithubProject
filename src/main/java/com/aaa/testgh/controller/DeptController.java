f5b0c40cea7a210bab897d9b8d9eb0a74ebe5e37
gwdqy88 <1649505635@qq.com>
2018-12-04 14:16:55 +0800

    新加了DeptController

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -9,6 +9,7 @@ package com.aaa.testgh.controller;
 
 public class DeptController {
     /**
+     * 先在Dev分支改 在add commit到本地仓库 在切换到master分支 然后去
      * 红色   绿色   白色
      * 未进缓冲区  进入缓冲区未提交  提交之后
      * @param args
