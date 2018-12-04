b6b318bb07f0231b86bb52f24e803a6ffc75e3b1
gwdqy88 <1649505635@qq.com>
2018-12-04 14:33:31 +0800

    新加了DeptController

--- src/main/java/com/aaa/testgh/controller/DeptController.java
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -9,7 +9,8 @@ package com.aaa.testgh.controller;
 
 public class DeptController {
     /**
-     * 先在Dev分支改 在add commit到本地仓库 在切换到master分支 然后去
+     * Dev分支只到commit push 合并到master之后才提交到远程仓库
+     * 先在Dev分支改 在add commit到本地仓库 在切换到master分支 然后去合并分支 在push到远程仓库
      * 红色   绿色   白色
      * 未进缓冲区  进入缓冲区未提交  提交之后
      * @param args
